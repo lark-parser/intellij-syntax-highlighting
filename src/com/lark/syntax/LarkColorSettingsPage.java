34652a2fa1d9aa594000b06996a07aced217fb48
MegaIng1 <trampchamp@hotmail.de>
2019-06-02 17:08:37 +0200

    - Added Basic Syntax Highlighting

---
+++ src/com/lark/syntax/LarkColorSettingsPage.java
@@ -0,0 +1,78 @@
+package com.lark.syntax;
+
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.options.colors.AttributesDescriptor;
+import com.intellij.openapi.options.colors.ColorDescriptor;
+import com.intellij.openapi.options.colors.ColorSettingsPage;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.Map;
+
+public class LarkColorSettingsPage implements ColorSettingsPage {
+    private static final AttributesDescriptor[] DESCRIPTORS = new AttributesDescriptor[]{
+            new AttributesDescriptor("Rule Name", LarkSyntaxHighlighter.RULE),
+            new AttributesDescriptor("Token Name", LarkSyntaxHighlighter.TOKEN),
+            new AttributesDescriptor("Assign Colon", LarkSyntaxHighlighter.ASSIGN),
+            new AttributesDescriptor("Alias Arrow", LarkSyntaxHighlighter.ALIAS),
+            new AttributesDescriptor("Quantifiers", LarkSyntaxHighlighter.QUANTIFIER),
+            new AttributesDescriptor("Keyword", LarkSyntaxHighlighter.KEYWORD),
+            new AttributesDescriptor("String", LarkSyntaxHighlighter.STRING),
+            new AttributesDescriptor("Regexp", LarkSyntaxHighlighter.REGEXP),
+            new AttributesDescriptor("Comment", LarkSyntaxHighlighter.COMMENT),
+    };
+
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public SyntaxHighlighter getHighlighter() {
+        return new LarkSyntaxHighlighter();
+    }
+
+    @NotNull
+    @Override
+    public String getDemoText() {
+        return "// A demo-lark-grammar\n" +
+                "start: \"test\" data+\n" +
+                "data: _SEPARATOR (entry|field)+\n" +
+                "_SEPARATOR: /[:=\\s]+/\n" +
+                "entry: NAME -> name\n" +
+                "     | SIGNED_NUMBER -> number\n" +
+                "field: entry \"|\" entry\n" +
+                "%import SIGNED_NUMBER\n" +
+                "%import CNAME -> NAME\n" +
+                "%import NEWLINE -> NL\n" +
+                "%ignore NL\n";
+    }
+
+    @Nullable
+    @Override
+    public Map<String, TextAttributesKey> getAdditionalHighlightingTagToDescriptorMap() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public AttributesDescriptor[] getAttributeDescriptors() {
+        return DESCRIPTORS;
+    }
+
+    @NotNull
+    @Override
+    public ColorDescriptor[] getColorDescriptors() {
+        return ColorDescriptor.EMPTY_ARRAY;
+    }
+
+    @NotNull
+    @Override
+    public String getDisplayName() {
+        return "Lark";
+    }
+}
--- src/com/lark/syntax/LarkSyntaxHighlighter.java
+++ src/com/lark/syntax/LarkSyntaxHighlighter.java
@@ -1,4 +1,92 @@
 package com.lark.syntax;
 
-public class LarkSyntaxHighlighter {
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.HighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
+import com.lark.syntax.psi.LarkTypes;
+import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+
+public class LarkSyntaxHighlighter extends SyntaxHighlighterBase {
+    public static final TextAttributesKey KEYWORD = createTextAttributesKey("LARK_KEYWORD",
+            DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey ASSIGN = createTextAttributesKey("LARK_ASSIGN",
+            DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    public static final TextAttributesKey ALIAS = createTextAttributesKey("LARK_ALIAS",
+            DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    public static final TextAttributesKey QUANTIFIER = createTextAttributesKey("LARK_QUANTIFIER",
+            DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    public static final TextAttributesKey STRING = createTextAttributesKey("LARK_STRING",
+            DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey REGEXP = createTextAttributesKey("LARK_REGEXP",
+            DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey RULE = createTextAttributesKey("LARK_RULE",
+            DefaultLanguageHighlighterColors.LOCAL_VARIABLE);
+    public static final TextAttributesKey TOKEN = createTextAttributesKey("LARK_TOKEN",
+            DefaultLanguageHighlighterColors.GLOBAL_VARIABLE);
+    public static final TextAttributesKey COMMENT = createTextAttributesKey("LARK_COMMENT",
+            DefaultLanguageHighlighterColors.LINE_COMMENT);
+    public static final TextAttributesKey BAD_CHARS = createTextAttributesKey("LARK_BAD_CHARS",
+            HighlighterColors.BAD_CHARACTER);
+    private static final TextAttributesKey[] KEYWORD_KEYS = new TextAttributesKey[]{KEYWORD};
+    private static final TextAttributesKey[] ASSIGN_KEYS = new TextAttributesKey[]{ASSIGN};
+    private static final TextAttributesKey[] ALIAS_KEYS = new TextAttributesKey[]{ALIAS};
+    private static final TextAttributesKey[] QUANTIFIER_KEYS = new TextAttributesKey[]{QUANTIFIER};
+    private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING};
+    private static final TextAttributesKey[] REGEXP_KEYS = new TextAttributesKey[]{REGEXP};
+    private static final TextAttributesKey[] RULE_KEYS = new TextAttributesKey[]{RULE};
+    private static final TextAttributesKey[] TOKEN_KEYS = new TextAttributesKey[]{TOKEN};
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARS};
+    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+
+    @NotNull
+    @Override
+    public Lexer getHighlightingLexer() {
+        return new LarkLexerAdapter();
+    }
+
+    @NotNull
+    @Override
+    public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
+        if (tokenType.equals(LarkTypes.COLON)) {
+            return ASSIGN_KEYS;
+        } else if (tokenType.equals(LarkTypes.IGNORE) ||
+                tokenType.equals(LarkTypes.DECLARE) ||
+                tokenType.equals(LarkTypes.IMPORT)) {
+            return KEYWORD_KEYS;
+        } else if (tokenType.equals(LarkTypes.QUANT)) {
+            return QUANTIFIER_KEYS;
+        } else if (tokenType.equals(LarkTypes.STRING)) {
+            return STRING_KEYS;
+        } else if (tokenType.equals(LarkTypes.REGEXP)) {
+            return REGEXP_KEYS;
+        } else if (tokenType.equals(LarkTypes.COMMENT)) {
+            return COMMENT_KEYS;
+        } else if (tokenType.equals(LarkTypes.RULE)) {
+            return RULE_KEYS;
+        } else if (tokenType.equals(LarkTypes.TOKEN)) {
+            return TOKEN_KEYS;
+        } else if (tokenType.equals(LarkTypes.ARROW)) {
+            return ALIAS_KEYS;
+        } else if (tokenType.equals(TokenType.BAD_CHARACTER)) {
+            return BAD_CHAR_KEYS;
+        } else if (tokenType.equals(TokenType.WHITE_SPACE) ||
+                tokenType.equals(LarkTypes.NEWLINE) ||
+                tokenType.equals(LarkTypes.OP) ||
+                tokenType.equals(LarkTypes.CP) ||
+                tokenType.equals(LarkTypes.OB) ||
+                tokenType.equals(LarkTypes.CB)
+        ) {
+            return EMPTY_KEYS;
+        } else {
+            System.out.println(tokenType);
+            return EMPTY_KEYS;
+        }
+    }
 }
